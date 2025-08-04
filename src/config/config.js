d3d1016a796dd53c3d10b7bc58a3e10d7907c6ec
jimonasutaa <jimonasuta@gmail.com>
2025-08-04 10:24:17 +0700

    Update config.js
    try edit to deploy varcel

--- src/config/config.js
+++ src/config/config.js
@@ -8,5 +8,19 @@ module.exports = {
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
     logging: false
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
   }
 }; 
